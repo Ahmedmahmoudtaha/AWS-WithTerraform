a0ff9e187bc54d892c92dbd3a69edb60216758bd
“taha” <ahmedmtaha95@gmail.com>
2023-03-03 16:00:22 +0200

    Initial commit

--- Resources.tf
+++ Resources.tf
@@ -1,4 +1,12 @@
 
+terraform {
+    backend "s3" {
+    bucket = "taha-bucket-s3-iti"
+    key    = "terraform-taha-statefile"
+    region = "us-east-2"
+  }
+}
+
 
 
 resource "aws_instance" "my-ec2"{
